dac044e00891be7ef857440040ac4fa593d581b2
PC <galocoriamaio@gmail.com>
2025-08-06 14:51:48 -0300

    Ejercicios terminados

--- ProyectosJava/JavaApplication2/src/Ejercicio6.java
+++ ProyectosJava/JavaApplication2/src/Ejercicio6.java
@@ -8,5 +8,10 @@
  * @author PC
  */
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+    int x = 10; // Línea 1
+    x = x + 5; // Línea 2
+    System.out.println(x); // Línea 3
+//    x + 5 y x = 10 es una expresion y una instruccion es System.out.println(x)
+    }
 }
